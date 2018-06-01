3dd1426a721415cd11f13b5c3af83007d9f68439
Daniel Tamayo <datamayo@utp.edu.co>
2018-06-01 14:15:43 -0500

    se conectan nodos y actualizan finger tables con ip y puerto

--- hash/hash.py
+++ hash/hash.py
@@ -40,6 +40,16 @@ def getFingerTable(nodes,id):
 
     return temp
 
+def equal(a,b):    
+    if b==None or len(a) != len(b):
+        return False
+    else:
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                return False 
+    return True
+
+
 def main():
     if len(sys.argv) != 2:
         print("Invalid port")
@@ -51,28 +61,36 @@ def main():
     port= sys.argv[1]
     socket.bind("tcp://*:{}".format(port))
     print("Started server")
-
     nodes={} #asocia key con tupla de ip y puerto
     fingers={} #asocia key con fingerTable
 
     while True:
         ident, op , *args = socket.recv_multipart()
+        print(ident," ",op," ",args)
         ident=ident.decode()
         
         if op.decode()=="add":            
             if not validKey(nodes,ident):
                 socket.send_multipart([bytes(ident,'ascii') ,bytes("refused", 'ascii')])
             else:
+                print('loading')
                 nodes[ident]=(args[0].decode(),args[1].decode())
-                fingers[ident]=getFingerTable(nodes,ident)
-                msg=[bytes(ident,'ascii'),bytes("acepted", 'ascii')]
-                temp=[]
-                for finger in fingers[ident]:
-                    temp.append(bytes(str(finger),'ascii'))
-                msg+=temp
-                print(msg)
-                socket.send_multipart(msg)
-                
+                print(ident," ",nodes[ident])
+                for key in nodes:
+                    if key in fingers:
+                        temp=fingers[key]
+                    else: temp=None
+                    fingers[key]=getFingerTable(nodes,key)
+                    if not equal(fingers[key],temp):
+                        msg=[bytes(key,'ascii'),bytes("update", 'ascii')]
+                        temp=[]
+                        for finger in fingers[key]:
+                            temp.append(bytes(str(finger)+" "+str(nodes[str(finger)][0])+" "+str(nodes[str(finger)][1]),'ascii'))
+                        msg+=temp
+                        print("msg: ",msg)
+                        socket.send_multipart(msg)
+                print('finishing')
+                    
                 
 
 if __name__ == '__main__':
--- hash/node.py
+++ hash/node.py
@@ -17,10 +17,33 @@ def main():
     serverSocket.identity = identity
     serverSocket.connect("tcp://{}:{}".format(serverIp, serverPort)) #corchetes por que se enviaron los parametros el ip y el port
     serverSocket.send_multipart([bytes('add', 'ascii'),bytes(myIp,'ascii'),bytes(myPort,'ascii')])
-    ans = serverSocket.recv_multipart()
-    print(ans)
+    
     print("Started node with id {}".format(identity))
     
+    poller = zmq.Poller()
+    poller.register(sys.stdin, zmq.POLLIN)
+    poller.register(serverSocket, zmq.POLLIN)
+
+    fingerTable={}
+
+    while True:
+        socks = dict(poller.poll())
+        if serverSocket in socks:
+            op , *msg= serverSocket.recv_multipart()
+            print(op,' ',msg)
+            op = op.decode()
+            if op == 'refused':
+                print ('connection refused')
+                break
+            if op == 'update':
+                print('new finger: ',msg)
+                fingerTable={}
+                for item in msg:
+                    temp=item.split()
+                    fingerTable[temp[0].decode()]=(temp[1].decode(),int(temp[2].decode()))
+                print(fingerTable)
+
+
     
 if __name__ == '__main__':
     main()
